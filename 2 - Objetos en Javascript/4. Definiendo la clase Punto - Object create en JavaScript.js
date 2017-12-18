0e9a3055a4c7f1aa01caaf0b9cdfe5368185df6b
willarevalo <willarevalo.developer@gmail.com>
2017-12-18 11:31:39 -0500

    Add lesson 4  module 2

--- 2 - Objetos en Javascript/4. Definiendo la clase Punto - Object create en JavaScript.js
+++ 2 - Objetos en Javascript/4. Definiendo la clase Punto - Object create en JavaScript.js
@@ -1,7 +1,7 @@
 //Prototipos son lo mas similar a las clases
 //Hallar la hipotenusa de un triangulo rectangulo a partir de la tangente
 // d = sqrt(x^(2)+y^(2))
-function Punto(x,y){
+/*function Punto(x,y){
 	//constructor
 	this.x = x
 	this.y = y
@@ -20,14 +20,15 @@ Punto.prototype.distancia = function distancia(p){
 	//p1.distancia(p)
 	//Algo un poco mas complejo pero se entiende si se ven los ejemplos anteriores
 	return Math.sqrt(Math.pow(this.x - p.x, 2) + Math.pow(this.y - p.y, 2)).toFixed(2)
-}
+}*/
+
 
 
 const p1 = new Punto(0, 4)
 const p2 = new Punto(3, 0)
 
 console.log(p1.distancia(p2))
-console.log(p2.distancia({x:20, y:-7}))
+console.log(p2.distancia(p1, {x:20, y:-7}))
 p1.moverEnX(10)
 console.log(p1)
 p2.moverEnY(20)
