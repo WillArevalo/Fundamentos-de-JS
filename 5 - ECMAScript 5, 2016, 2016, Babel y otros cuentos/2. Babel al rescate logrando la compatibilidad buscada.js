1fe5d29bda60f4e9b9db321608f2b989676408a4
willarevalo <willarevalo.developer@gmail.com>
2017-12-20 11:42:44 -0500

    add lesson 3 module 5

---
+++ 5 - ECMAScript 5, 2016, 2016, Babel y otros cuentos/2. Babel al rescate logrando la compatibilidad buscada.js
@@ -0,0 +1,10 @@
+/*Babel al rescate: logrando la compatibilidad buscada
+
+Babel
+
+Usa JavaScript de la próxima generación hoy mismo.
+
+Babel es una herramienta que te permite hacer compatible el código que usa funcionalidades de ECMAScript con los diferentes navegadores.
+
+Para poder utilizar Babel debes tener instalado Node.
+*/
---
+++ 5 - ECMAScript 5, 2016, 2016, Babel y otros cuentos/3. Distintas formas de escribir módulos en JavaScript.js
@@ -0,0 +1,11 @@
+/*Que es un modulo en javascript?
+
+Básicamente un modulo es un paquete de codigo. Un modulo es un pedazo de código encapsulado que se encarga de resolver alguna cosa en particular.
+
+Las razones por las cuales nosotros vamos a querer escribir un modulo son dos, la primera es encapsulamiento, todo lo que pasa en un pedazo de codigo tiene que ser local a ese pedazo de codigo, de esa manera cada parte del codigo se hace responsable de cumplir una tarea. NO es que hacemos un modulo que hace un monton de cosas , sino que cada modulo debe ser pequeño y encargarse de una sola responsabilidad.
+La segunda razon por la cual nosotros queremos escribir un modulo son las dependencias.
+
+Como crear un modulo
+
+Existen varias manera de escribir un modulo, como por ejemplo con commonjs, amd, y el nuevo metodo de ecmascript 2015.
+La manera de crear modulos usando el metodo de ecmascript 2015 es la manera mas utilizada y mas facil de implementar. Basicamente hacemos import y export en cada modulo. Por ejemplo si tenemos un modulo que se llama cuadrado que sirve para calcular cosas de un cuadrado podemos utiliar este modulo en otro modulo importandolo*/
