ae5e441c3f40a7e22340c01a16f8f685b74aab31
willarevalo <willarevalo.developer@gmail.com>
2017-12-21 12:11:53 -0500

    add lesson 1 module 6

---
+++ 6 - Procesos asincronos/1. No generes un cuello de botella en el EventLoop.js
@@ -0,0 +1,12 @@
+// No generes un cuello de botella en el EventLoop
+
+// ¿Cómo ejecuta JavaScript el código?
+
+// Lo primero que se ejecuta es la declaración de las funciones.
+// Cuando JavaScript ejecuta una línea la carga en una pila de ejecución.
+
+// JavaScript solo ejecuta una cosa a la vez, por esto, debemos tenerla lo más limpia posible.
+
+// Sin embargo, puede delegar la ejecución de otras cosas.
+//Ejemplo en esta web http://latentflip.com/loupe/
+// un video excelente para entender el tema https://www.youtube.com/watch?v=8aGhZQkoFbQ
